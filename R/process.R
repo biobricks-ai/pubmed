38af19dda7a7143b3d11e11ce2d0b45d1c94f448
Thomas Luechtefeld <tom@insilica.co>
2022-05-29 18:40:41 -0400

    pubmed working with logging

--- R/process.R
+++ R/process.R
@@ -1,11 +1,12 @@
+# DEPENDENCIES =======================================================================================
 library(fs)
 library(purrr)
-library(xml2,  include.only=c("read_xml","xml_children","as_list"))
-library(XML,   include.only=c("parseDTD"))
+library(xml2, include.only=c("read_xml","xml_children","as_list"))
+library(XML, include.only=c("parseDTD"))
 library(arrow, include.only=c("write_parquet"))
+library(future)
 library(furrr)
 
-gz <- fs::dir_ls("download/ftp.ncbi.nlm.nih.gov/pubmed/baseline",regexp=".gz$")
 el <- { # BUILD SCHEMA FOR XML ELEMENTS
   load.dtd <- \(path){ readLines(path) |> paste(col="\n") |> XML::parseDTD(asText=T,error=\(...){})}
   el    <- load.dtd("download/pubmed/pubmed_190101.dtd")$elements
@@ -14,7 +15,13 @@ el <- { # BUILD SCHEMA FOR XML ELEMENTS
   map2(occur,child,~ list(occur=.x,child=.y)) |> set_names(names(el))
 }
 
+# TRANSFORM =======================================================================================
 process_file <- function(file,out,el){
+
+  mapchild <- function(node,setname){
+    children = xml2::xml_children(node)
+    purrr::map(children,mkjson) |> purrr::set_names(if(setname){ xml2::xml_name(children) }else{NULL})
+  }
 
   mkjson <- \(node){
     tag <- xml2::xml_name(node)
@@ -25,25 +32,26 @@ process_file <- function(file,out,el){
       T ~ { stop("illegal condition") })
   }
 
-  mapchild <- function(node,setname){
-    children = xml2::xml_children(node)
-    purrr::map(children,mkjson) |> purrr::set_names(if(setname){ xml2::xml_name(children) }else{NULL})
-  }
-
-  articles   <- xml2::read_xml(file) |> xml2::xml_children()
-  json       <- articles |> purrr::map_chr( ~ mkjson(.) |> jsonlite::toJSON(auto_unbox = T)) # HEAVY COMPUTE STEP
-  pmid       <- articles |> xml2::xml_find_all("MedlineCitation/PMID") |> xml2::xml_text()
-  tbl        <- tibble::tibble(pmid=pmid,json=json)
-  tbl$source <- fs::path_file(file)
+  art     <- xml2::read_xml(file) |> xml2::xml_children()
+  json    <- art |> map(mkjson) |> map_chr(jsonlite::toJSON,auto_unbox = T) # COMPUTE STEP
+  pmid    <- art |> xml2::xml_find_all("MedlineCitation/PMID") |> xml2::xml_text() |> as.integer()
+  tbl     <- tibble::tibble(pmid=pmid,json=json)
+  tbl$src <- fs::path_file(file)
   
   arrow::write_parquet(tbl,out)
 }
 
 # OUTS =================================================================================
-outfile  <- fs::dir_create("data/pubmed.parquet/") |>
-  fs::path(fs::path_file(gz) |> fs::path_ext_remove() |> fs::path_ext_set("parquet")) |>
-  fs::path_abs()
+gzfile  <- fs::dir_ls("download/ftp.ncbi.nlm.nih.gov/pubmed/baseline",regexp=".gz$")
+outdir  <- fs::dir_create("data/pubmed.parquet/") |> fs::path_abs()
+outfile <- fs::path(outdir,fs::path_file(gzfile)) |> gsub(pat="xml.gz$",repl="parquet") 
 
-library(future)
-plan(multisession)
-furrr::future_walk2(gz, outfile, process_file, el=el)
+# PROCESS WITH LOGGING
+step <- \(i,o,el){
+  res      <- safely(process_file)(i,o,el) 
+  err_path <- fs::path("log",fs::path_file(i) |> fs::path_ext_set("error"))
+  map_chr(res$error,jsonlite::toJSON,force=T) |> walk(write,file=errpath)
+}
+
+plan(multisession(workers = 15))
+furrr::future_walk2(gzfile, outfile, step, el=el)
---
+++ status.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+outf   <- fs::dir_ls(fs::path("data/pubmed.parquet/"))
+numf   <- length(fs::dir_ls("download/ftp.ncbi.nlm.nih.gov/pubmed/baseline",regexp=".gz$"))
+done   <- length(outf)
+first  <- fs::file_info(outf) |> dplyr::pull(modification_time) |> min() |> lubridate::as_datetime()
+
+total_secs  <- lubridate::interval(first,lubridate::now()) |> lubridate::as.difftime() |> lubridate::time_length() 
+min_per_doc <- (total_secs/done) / 60
+est_hour    <- numf * min_per_doc / 60
+
+failures <- fs::dir_ls("log") |> length()
+
+jsonlite::toJSON(list(
+  elapsed = total_secs,
+  doc_rem = numf - done,
+  doc_don = done,
+  failure = failures,
+  doc_min = round(1/min_per_doc,2),
+  est_hrs = round(est_hour,2)
+),auto_unbox = T,pretty = T)
